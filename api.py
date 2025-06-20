77d8ad0b8f2f54a69969666b5b75ce7e15b07bd8
causeri3 <vanessaca@web.de>
2025-06-20 23:48:55 +0200

    make arguments passable through api

--- api.py
+++ api.py
@@ -4,14 +4,35 @@ import io
 import numpy as np
 
 from PIL import Image
-from sdxlturbo import generate_image_list, PROMPT
+from sdxlturbo import (generate_image_list,
+                       NUM_INFERENCE_STEPS,
+                       STRENGTH_MIN,
+                       STRENGTH_MAX,
+                       GUIDANCE_SCALE,
+                       PROMPT,
+                       AMOUNT_PICS)
 
 app = FastAPI()
-
 @app.post("/sdxlturbo")
-async def generate(file: UploadFile, prompt: str = Form(PROMPT)):
+async def generate(
+    file: UploadFile,
+    prompt: str = Form(PROMPT),
+    amount_pics: int = Form(AMOUNT_PICS),
+    num_inference_steps: int = Form(NUM_INFERENCE_STEPS),
+    strength_min: float = Form(STRENGTH_MIN),
+    strength_max: float = Form(STRENGTH_MAX),
+    guidance_scale: int = Form(GUIDANCE_SCALE)
+):
     pil_image = Image.open(file.file).convert("RGB")
-    images = generate_image_list(pil_image, prompt=prompt)
+    images = generate_image_list(
+        pil_image,
+        prompt=prompt,
+        amount_pics=amount_pics,
+        num_inference_steps=num_inference_steps,
+        strength_min=strength_min,
+        strength_max=strength_max,
+        guidance_scale=guidance_scale
+    )
     numpy_images = np.stack([np.array(img) for img in images])
 
     buffer = io.BytesIO()
--- client_test.py
+++ client_test.py
@@ -4,19 +4,30 @@ import requests
 import numpy as np
 import io
 from PIL import Image
+import time
 
 your_file_path = r"Downloads/img1.jpg"
 
 files = {'file': open(your_file_path, 'rb')}
-data = {'prompt': 'dmt'}
+data = {
+    'prompt': 'dmt',
+    'amount_pics': 4,
+    'num_inference_steps': 15,
+    'strength_min': 0.1,
+    'strength_max': 0.5,
+    'guidance_scale': 4
+}
 
-response = requests.post("http://localhost:8000/sdxlturbo",
+start = time.time()
+response = requests.post(
+    "http://localhost:8000/sdxlturbo",
                          files=files,
                          data=data,
                          timeout=600)
 
 buffer = io.BytesIO(response.content)
 images = np.load(buffer)
+print(f"took {time.time() - start} sec ")
 
 
 for idx, img_array in enumerate(images):
